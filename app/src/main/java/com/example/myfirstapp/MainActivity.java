20668ee39c71cd0c71232c568459d3d9c11cb6cd
hyunOO <khw7147@kaist.ac.kr>
2017-06-30 19:03:55 +0900

    button1

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn01 = (Button)findViewById(R.id.button01);
-
         Button btn02 = (Button)findViewById(R.id.button02);
         Button btn03 = (Button)findViewById(R.id.button03);
 
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.button01);
+            }
+        });
     }
 }
