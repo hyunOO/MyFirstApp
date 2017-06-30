e30e1ea85f1bca387f91524d1db0366ac74a53f9
sumin shin <ssumin6@gmail.com>
2017-06-30 19:27:01 +0900

    add a home button in tab a. and make intent switching between activities

--- app/src/main/java/com/example/myfirstapp/TabA.java
+++ app/src/main/java/com/example/myfirstapp/TabA.java
@@ -1,7 +1,11 @@
 package com.example.myfirstapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
 public class TabA extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class TabA extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
+
+        Button home = (Button)findViewById(R.id.button);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
