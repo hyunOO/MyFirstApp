5c09f2df269752af8bf580ea67e03548db04848d
sumin shin <ssumin6@gmail.com>
2017-06-30 20:35:00 +0900

    tab b changed

--- app/src/main/java/com/example/myfirstapp/TabB.java
+++ app/src/main/java/com/example/myfirstapp/TabB.java
@@ -1,8 +1,13 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.database.Cursor;
+import android.widget.BaseAdapter;
 import android.view.View;
 
 public class TabB extends AppCompatActivity {
@@ -12,7 +17,21 @@ public class TabB extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_b);
 
-        GridView gv = (GridView)findViewById(R.id.gridView);
+        String[] img = {MediaStore.Images.Thumbnails._ID};
+        Cursor imageCursor = managedQuery(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,img, null,null,MediaStore.Images.Thumbnails.IMAGE_ID+"");
+        imageColumnIndex = imageCursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
 
+        gridAdapter adapter = new gridAdapter();
+        GridView gv = (GridView)findViewById(R.id.gridView);
+        gv.setAdapter(adapter);
+
+    }
+}
+public class gridAdapter extends BaseAdapter{
+
+    public gridAdapter(Context context){
+        super();
+
+        mContext = context;
     }
 }
