81c235cf52519112abac3c897a6720e84f29dfb9
sumin shin <ssumin6@gmail.com>
2017-07-01 11:39:08 +0900

    add adapter to tab b

--- app/src/main/java/com/example/myfirstapp/TabB.java
+++ app/src/main/java/com/example/myfirstapp/TabB.java
@@ -1,14 +1,16 @@
 package com.example.myfirstapp;
 
+
 import android.content.Context;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.database.Cursor;
-import android.widget.BaseAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class TabB extends AppCompatActivity {
 
@@ -17,21 +19,44 @@ public class TabB extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_b);
 
-        String[] img = {MediaStore.Images.Thumbnails._ID};
-        Cursor imageCursor = managedQuery(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,img, null,null,MediaStore.Images.Thumbnails.IMAGE_ID+"");
-        imageColumnIndex = imageCursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
-
-        gridAdapter adapter = new gridAdapter();
+        GalleryAdapter adapter = new GalleryAdapter(TabB.this, web, img);
         GridView gv = (GridView)findViewById(R.id.gridView);
         gv.setAdapter(adapter);
 
     }
 }
-public class gridAdapter extends BaseAdapter{
+public class GalleryAdapter extends BaseAdapter{
+    private Context mContext;
+    private final String[] mtable;
+    private final int[] imageid;
 
-    public gridAdapter(Context context){
+    public GalleryAdapter(Context context){
         super();
 
         mContext = context;
+    }
+    public GalleryAdapter(Context context, String[] table, int[] image){
+        mContext = context;
+        mtable = table;
+        imageid=image;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent){
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null){
+            gridView = inflater.inflate(R.layout.image_view, null);
+            ImageView imageView = (ImageView)gridView.findViewById(R.id.imageView);
+            imageView.setImageResource(imageid[position]);
+        }else{
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+
+    public int getCount() {
+        return mtable.length;
     }
 }
