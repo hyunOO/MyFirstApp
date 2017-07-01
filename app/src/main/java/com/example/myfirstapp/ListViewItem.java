3337f22f69a8253f1df1365cca51124ddb104a45
hyunOO <khw7147@kaist.ac.kr>
2017-07-01 12:37:41 +0900

    listview를 지정해서 보여주게 하였다

---
+++ app/src/main/java/com/example/myfirstapp/ListViewItem.java
@@ -0,0 +1,22 @@
+package com.example.myfirstapp;
+
+/**
+ * Created by q on 2017-07-01.
+ */
+
+public class ListViewItem {
+    private String text1, text2;
+
+    public void setText1(String text1){
+        this.text1 = text1;
+    }
+    public void setText2(String text2){
+        this.text2 = text2;
+    }
+    public String getText1(){
+        return this.text1;
+    }
+    public String getText2(){
+        return this.text2;
+    }
+}
