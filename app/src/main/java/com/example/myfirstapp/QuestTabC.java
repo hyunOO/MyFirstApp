ddbf56053a0bf49ad00ade05ba8f65306cb05c3a
hyunOO <khw7147@kaist.ac.kr>
2017-07-03 16:46:41 +0900

    일단 블루투스 주석처리

---
+++ app/src/main/java/com/example/myfirstapp/QuestTabC.java
@@ -0,0 +1,52 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by q on 2017-07-03.
+ */
+
+public class QuestTabC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn01 = (Button)findViewById(R.id.button01);
+        Button btn02 = (Button)findViewById(R.id.button02);
+        Button btn03 = (Button)findViewById(R.id.button03);
+
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TabA.class);
+                startActivity(intent);
+            }
+        });
+
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TabB.class);
+                startActivity(intent);
+            }
+        });
+
+        btn03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), TabC.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+}
--- app/src/main/java/com/example/myfirstapp/TabC.java
+++ app/src/main/java/com/example/myfirstapp/TabC.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -18,27 +20,35 @@ public class TabC extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_c);
+/*
+        final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        BluetoothDevice mRemoteDevice;
-        BluetoothSocket mSocket = null;
-        OutputStream mOutputStream = null;
-        InputStream mInputStream = null;
-
-        if(!mBluetoothAdapter.isEnabled()){
-            AlertDialog.Builder alert = new AlertDialog.Builder(TabC.this);
-            alert.setTitle("앱 실행 오류");
-            alert.setMessage("블루투스를 활성화하세요.");
-            alert.show();
-        }
-
-        else{
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            for(BluetoothDevice device : pairedDevices){
-                device.getName();
-                device.getAddress();
+        Button start_button = (Button) findViewById(R.id.start_game);
+        start_button.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!mBluetoothAdapter.isEnabled()){
+                    AlertDialog.Builder alert = new AlertDialog.Builder(TabC.this);
+                    alert.setTitle("앱 실행 오류");
+                    alert.setMessage("블루투스를 활성화하세요.");
+                    alert.show();
+                }
+                else{
+                    boolean b = false;
+                    // b가 true이면 출제자
+                    if(b){
+                        Intent intent1 = new Intent(getApplicationContext(), QuestTabC.class);
+                        startActivity(intent1);
+                    }
+                    // b가 false이면 답안자
+                    else{
+                        Intent intent2 = new Intent(getApplicationContext(), AnswerTabC.class);
+                        startActivity(intent2);
+                    }
+                }
             }
-        }
+        }));
+        */
     }
     @Override
     public void onBackPressed(){
