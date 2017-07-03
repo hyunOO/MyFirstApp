48892ef7c35a57e3a0f0fdbbae1783f896ead0b6
sumin shin <ssumin6@gmail.com>
2017-07-03 22:13:52 +0900

    사진 파일을 더 업로드했다.

--- app/src/main/java/com/example/myfirstapp/TabB.java
+++ app/src/main/java/com/example/myfirstapp/TabB.java
@@ -14,7 +14,7 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 public class TabB extends AppCompatActivity {
-    int[] imgid ={R.drawable.img20,R.drawable.img19,R.drawable.img18, R.drawable.img17, R.drawable.img16,R.drawable.img15,R.drawable.img14, R.drawable.img13,R.drawable.img12, R.drawable.img11,R.drawable.img10, R.drawable.img9,R.drawable.img1, R.drawable.img2, R.drawable.img3,R.drawable.img4, R.drawable.img5,R.drawable.img6, R.drawable.img7};
+    int[] imgid ={R.drawable.img20,R.drawable.img19,R.drawable.img18, R.drawable.img17, R.drawable.img16,R.drawable.img15,R.drawable.img12, R.drawable.img11,R.drawable.img10, R.drawable.img9,R.drawable.img1, R.drawable.img2, R.drawable.img3,R.drawable.img4, R.drawable.img5,R.drawable.img6, R.drawable.img7};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
