b105b1b82b63fb8ee32b1128b6c7f08f489c76ba
sumin shin <ssumin6@gmail.com>
2017-07-04 22:37:09 +0900

    tab b수정중

--- app/src/main/java/com/example/myfirstapp/Image.java
+++ app/src/main/java/com/example/myfirstapp/Image.java
@@ -2,7 +2,10 @@ package com.example.myfirstapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,23 +17,15 @@ import android.widget.TextView;
 
 public class Image extends Activity {
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
 
-        ImageView iv = (ImageView)findViewById(R.id.imageView3);
-        TextView show_name = (TextView)findViewById(R.id.textView9);
-        Button btn = (Button)findViewById(R.id.button);
+        ImageView iv = (ImageView) findViewById(R.id.imageView3);
+        TextView show_name = (TextView) findViewById(R.id.textView9);
+        Button btn = (Button) findViewById(R.id.button);
 
         Intent intent = getIntent();
-        iv.setImageResource(intent.getIntExtra("img",0));
+        iv.setImageResource(intent.getIntExtra("img", 0));
         show_name.setText(intent.getStringExtra("name"));
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-}
-}
+    }}
