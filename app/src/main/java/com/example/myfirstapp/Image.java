91697e75ac0f91df9839d067a425887939a36482
sumin shin <ssumin6@gmail.com>
2017-07-04 22:21:20 +0900

    ui 수정

--- app/src/main/java/com/example/myfirstapp/Image.java
+++ app/src/main/java/com/example/myfirstapp/Image.java
@@ -3,6 +3,8 @@ package com.example.myfirstapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,9 +20,17 @@ public class Image extends Activity {
 
         ImageView iv = (ImageView)findViewById(R.id.imageView3);
         TextView show_name = (TextView)findViewById(R.id.textView9);
+        Button btn = (Button)findViewById(R.id.button);
 
         Intent intent = getIntent();
         iv.setImageResource(intent.getIntExtra("img",0));
         show_name.setText(intent.getStringExtra("name"));
-    }
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+}
 }
